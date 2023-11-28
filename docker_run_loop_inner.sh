1b646d3b83bfd219f1546c0640cbb010e242aeae
Gianluca Carloni <91902479+gianlucarloni@users.noreply.github.com>
2023-11-28 12:16:57 +0100

    cleaned version

--- docker_run_loop_inner.sh
+++ docker_run_loop_inner.sh
@@ -1,66 +1,121 @@
 #!/bin/bash
 
-#training convnet in a loop setting to test different hyperparameters: note that trainings have earlystopping criterion
-## GPU ids to be used:
-arrVar="0"
+#This bash script is used to run the causality-driven and baseline versions of the networks with all the hyperparameters (optimization) in a loop (inside the python script)
+# Indeed, it executes a docker run command which utilizes the conv:train_convnet_loop_inner_B2 docker image. Please customise the arguments below.
 
-# arrVar="0,3,5,7"
-# arrVar="1,4,6,7"
-
-# arrVar="0"
-
-### Causality parameters
-CAUSALITY_SETTING="['mulcatbool']" #"['cat','mulcatbool','mulcat']"
-CAUSALITY_AWARENESS_METHOD="['lehmer']" #"['lehmer','max',None]"
-LEHMER_PARAM="[1]" #"[-100,-2,-1,0,1,100]"
-# added 21 luglio
-MULCAT_CAUSES_OR_EFFECTS="['causes']" # just one, that is valid for both, to run the ablation studies
-
-
-#### Hyperparameters tuning
-ADAM_LR="[0.001]" #"[0.01,0.001]"
-WD="[0.01]" #"[0.01,0.001,0.0001]"
-
-
-## fixed (for now) arguments
-NUM_GPUS=$(($1))
-SEED=42
-EXPERIMENT="prostate" #imagenette| prostate
-CONDITIONING_FEATURE="disease_yes_no" #imagenette | disease_yes_no, aggressiveness, no_tumour, scanner_vendor
-IMAGE_SIZE=128 #256 #96   #128 #64 #imagenette 64, prostate 128
-BATCH_SIZE_TRAIN=50 #20 #200 #200
+################# EXPERIMENT SETTINGS ################
+# Set the random seed:
+SEED=0 #42,1,0,2
+# Hyperparameters tuning
+LR="[1e-5]" #"[0.008,0.003]" #"[0.01,0.001]"
+WD="[1e-4]" #"[0.01,0.001,0.0001]"
+# Causality parameters
+CAUSALITY_SETTING="['mulcatbool','mulcat','cat']"
+CAUSALITY_AWARENESS_METHOD="['max',None,'lehmer']" #'None' is the baseline
+LEHMER_PARAM="[-2,-1,0,1,2]" #"[-100,-2,-1,0,1,100]"
+MULCAT_CAUSES_OR_EFFECTS="['causes','effects']"
+## other arguments
+EXPERIMENT="breakhis" #Depending on the dataset: imagenette| prostate | procancer | breakhis
+CONDITIONING_FEATURE="aggressiveness" #imagenette | disease_yes_no, aggressiveness, no_tumour, scanner_vendor
+IMAGE_SIZE=128 #96 #224
+BATCH_SIZE_TRAIN=12 #24
 BATCH_SIZE_VALID=1
 BATCH_SIZE_TEST=1
-NUMBER_OF_EPOCHS=300 #200, 100
-MODEL_TYPE="resnext" #eg, EqualCNN, resnet18, resnext_base          alexnet, SimpleCNN, EqualCNN, BaseCNN, LightCNN, , resnet34, resnet50
-which_resnext_to_use="tiny" #eg, "tiny", "base"
+NUMBER_OF_EPOCHS=150 #200
+MODEL_TYPE="resnet18" #eg, resnet18, resnext        [alexnet, SimpleCNN, EqualCNN, BaseCNN, LightCNN, , resnet34, resnet50]
+which_resnext_to_use="tiny" #eg, "tiny", "base" it is used only when MODEL_TYPE="resnext"
+is_pretrained=True
 
 
-echo "The top $NUM_GPUS free GPUs are: $arrVar"
+
+
+
+
+
+
+
+################# SYSTEM GPU/CPU SETTINGS: ################################################################
+## The first argument ($1) passed after the bash script corresponds to the number of desired GPU devices to be used, e.g., 1 or 3.
+NUM_GPUS=$(($1))
+
+## CHOOSE ONE OF THE TWO FOLLOWING SECTIONS ################################
+
+################ SECTION a) If you use regular (physical) GPU devices, then use this one and comment the other:
+arrVar="5" #e.g., "1,2,5" ## GPU ids to be used (single int, or string list of integers)
+## create new variables with a specific string formatting necessary to use it in Docker command
 arrVar2="device="$arrVar
 arrVar3="\""$arrVar2"\""
-NUM_CPUS=$((23*$NUM_GPUS))
-
-
-
+NUM_CPUS=$((24*$NUM_GPUS)) #generally, it is recommended to use 16-24 times the number of utilized GPUs
+echo "Requested $NUM_GPUS GPU(s), utilizing the one(s) with ID: $arrVar"
 docker run \
     --interactive --tty \
     --rm \
+    --user $(id -u):$(id -g) \
     --cpus $NUM_CPUS \
     --gpus $arrVar3 \
     --volume $PWD:$PWD \
     --workdir $PWD \
-    gianlucacarloni/conv:train_convnet_loop_inner_B2 $NUM_GPUS $arrVar $SEED $EXPERIMENT $CONDITIONING_FEATURE $IMAGE_SIZE $BATCH_SIZE_TRAIN $BATCH_SIZE_VALID $BATCH_SIZE_TEST $NUMBER_OF_EPOCHS $MODEL_TYPE $ADAM_LR $WD $CAUSALITY_AWARENESS_METHOD $LEHMER_PARAM $CAUSALITY_SETTING $MULCAT_CAUSES_OR_EFFECTS --which_resnext_to_use $which_resnext_to_use
+    gianlucacarloni/conv:train_convnet_loop_inner_B2 \
+                                                    --number_of_gpus $NUM_GPUS \
+                                                    --gpus_ids $arrVar \
+                                                    --SEED $SEED \
+                                                    --EXPERIMENT $EXPERIMENT \
+                                                    --CONDITIONING_FEATURE $CONDITIONING_FEATURE \
+                                                    --IMAGE_SIZE $IMAGE_SIZE \
+                                                    --BATCH_SIZE_TRAIN $BATCH_SIZE_TRAIN \
+                                                    --BATCH_SIZE_VALID $BATCH_SIZE_VALID \
+                                                    --BATCH_SIZE_TEST $BATCH_SIZE_TEST \
+                                                    --NUMBER_OF_EPOCHS $NUMBER_OF_EPOCHS \
+                                                    --MODEL_TYPE $MODEL_TYPE \
+                                                    --LR $LR \
+                                                    --WD $WD \
+                                                    --CAUSALITY_AWARENESS_METHOD $CAUSALITY_AWARENESS_METHOD \
+                                                    --LEHMER_PARAM $LEHMER_PARAM \
+                                                    --CAUSALITY_SETTING $CAUSALITY_SETTING \
+                                                    --MULCAT_CAUSES_OR_EFFECTS $MULCAT_CAUSES_OR_EFFECTS \
+                                                    --which_resnext_to_use $which_resnext_to_use \
+                                                    --is_pretrained $is_pretrained
+
+
+################ SECTION b) Else, if you want to use NVIDIA Multi-Instance GPU (MIG)(www.nvidia.com/it-it/technologies/multi-instance-gpu/)
+## i.e., some physical GPUs are replaced by 'virtual' ones typically used to execute smaller jobs, then you need
+## to find their GPU ID with the command 'nvidia-smi -L'.
+## E.g., to set the MIG GPU with ID "MIG-5e802a29-4df5-573c-bcad-d27aa83a4bdc", we set the arrVar (bash) to that value:
+# arrVar="MIG-5e802a29-4df5-573c-bcad-d27aa83a4bdc" 
+## create new variables with a specific string formatting necessary to use it in Docker command
+# arrVar2="device="$arrVar
+# arrVar3="\""$arrVar2"\""
+# NUM_CPUS=$((24*$NUM_GPUS)) #generally, it is recommended to use 16-24 times the number of utilized GPUs
+# echo "Requested $NUM_GPUS GPU(s), utilizing the one(s) with ID: $arrVar"
+##In this case, we need to specify the flags --runtime nvidia and -e NVIDIA_VISIBLE_DEVICE=$arrVar3, in the docker run command 
+# docker run \
+#     --interactive --tty \
+#     --rm \
+#     --user $(id -u):$(id -g) \
+#     --cpus $NUM_CPUS \
+#     --runtime nvidia \
+#     -e NVIDIA_VISIBLE_DEVICE=$arrVar3 \
+#     --volume $PWD:$PWD \
+#     --workdir $PWD \
+#     gianlucacarloni/conv:train_convnet_loop_inner_B2 \
+#                                                     --number_of_gpus $NUM_GPUS \
+#                                                     --gpus_ids $arrVar \
+#                                                     --SEED $SEED \
+#                                                     --EXPERIMENT $EXPERIMENT \
+#                                                     --CONDITIONING_FEATURE $CONDITIONING_FEATURE \
+#                                                     --IMAGE_SIZE $IMAGE_SIZE \
+#                                                     --BATCH_SIZE_TRAIN $BATCH_SIZE_TRAIN \
+#                                                     --BATCH_SIZE_VALID $BATCH_SIZE_VALID \
+#                                                     --BATCH_SIZE_TEST $BATCH_SIZE_TEST \
+#                                                     --NUMBER_OF_EPOCHS $NUMBER_OF_EPOCHS \
+#                                                     --MODEL_TYPE $MODEL_TYPE \
+#                                                     --LR $LR \
+#                                                     --WD $WD \
+#                                                     --CAUSALITY_AWARENESS_METHOD $CAUSALITY_AWARENESS_METHOD \
+#                                                     --LEHMER_PARAM $LEHMER_PARAM \
+#                                                     --CAUSALITY_SETTING $CAUSALITY_SETTING \
+#                                                     --MULCAT_CAUSES_OR_EFFECTS $MULCAT_CAUSES_OR_EFFECTS \
+#                                                     --which_resnext_to_use $which_resnext_to_use \
+#                                                     --is_pretrained $is_pretrained
     
-    # gianlucacarloni/conv:train_convnet_loop_inner_B2_ablation $NUM_GPUS $arrVar $SEED $EXPERIMENT $CONDITIONING_FEATURE $IMAGE_SIZE $BATCH_SIZE_TRAIN $BATCH_SIZE_VALID $BATCH_SIZE_TEST $NUMBER_OF_EPOCHS $MODEL_TYPE $ADAM_LR $WD $CAUSALITY_AWARENESS_METHOD $LEHMER_PARAM $CAUSALITY_SETTING $MULCAT_CAUSES_OR_EFFECTS
-    #gianlucacarloni/conv:train_convnet_loop_inner_B $NUM_GPUS $arrVar $SEED $EXPERIMENT $CONDITIONING_FEATURE $IMAGE_SIZE $BATCH_SIZE_TRAIN $BATCH_SIZE_VALID $BATCH_SIZE_TEST $NUMBER_OF_EPOCHS $MODEL_TYPE $ADAM_LR $WD $CAUSALITY_AWARENESS_METHOD $LEHMER_PARAM $CAUSALITY_SETTING
-    #gianlucacarloni/conv:train_convnet_loop_inner $NUM_GPUS $arrVar $SEED $EXPERIMENT $CONDITIONING_FEATURE $IMAGE_SIZE $BATCH_SIZE_TRAIN $BATCH_SIZE_VALID $BATCH_SIZE_TEST $NUMBER_OF_EPOCHS $MODEL_TYPE $ADAM_LR $WD $CAUSALITY_AWARENESS_METHOD $LEHMER_PARAM 
-    #gianlucacarloni/conv:train_convnet_loop_inner_dict $NUM_GPUS $arrVar $SEED $EXPERIMENT $CONDITIONING_FEATURE $IMAGE_SIZE $BATCH_SIZE_TRAIN $BATCH_SIZE_VALID $BATCH_SIZE_TEST $NUMBER_OF_EPOCHS $MODEL_TYPE "[0.001]" "['lehmer']" "[1]"
-
-#     gianlucacarloni/conv:train_convnet_loop_inner $NUM_GPUS $arrVar $SEED $EXPERIMENT $CONDITIONING_FEATURE $IMAGE_SIZE $BATCH_SIZE_TRAIN $BATCH_SIZE_VALID $BATCH_SIZE_TEST $NUMBER_OF_EPOCHS $MODEL_TYPE "[0.001]" "['lehmer']" "[-2]" 
-#$ADAM_LR $CAUSALITY_AWARENESS_METHOD $LEHMER_PARAM
-
-# --user $(id -u):$(id -g) \ ##sarebbero uid=1021(gianlucacarloni) gid=1021(gianlucacarloni)
-
 echo "END OF ALL TRAINING COMBINATIONS (LOOP)"
-#gianlucacarloni/conv:train_convnet $NUM_GPUS $arrVar
